9861c6dc28e49842367d06b2a641d45dd52bfa05
Prerna-Chauhan31 <prernaa3101@gmail.com>
2026-09-03 21:47:45 +0900

    Use environment variable for database URL

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://localhost/notesdb"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
